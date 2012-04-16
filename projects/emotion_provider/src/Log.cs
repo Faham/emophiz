76d89dfa3121d852b5208b213d01a0136449baba
Faham Negini <faham.negini@gmail.com>
2012-04-16 05:05:23 -0600

    loggin is now thread-safe

--- projects/emotion_provider/src/Log.cs
+++ projects/emotion_provider/src/Log.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace emophiz
 {
@@ -10,6 +11,7 @@ namespace emophiz
 		private string m_filename;
 		private System.IO.StreamWriter m_logfile;
 		private System.Collections.Concurrent.ConcurrentQueue<string> m_messages;
+		private System.Timers.Timer m_timer = new System.Timers.Timer(100);
 
 		public enum Priority
 		{
@@ -22,16 +24,31 @@ namespace emophiz
 		public Log(string filname = "Emotion.Log")
 		{
 			m_filename = filname;
+			m_logfile = new System.IO.StreamWriter(m_filename, false);
+			m_logfile.AutoFlush = true;
 			m_messages = new System.Collections.Concurrent.ConcurrentQueue<string>();
+			m_timer.Elapsed += new System.Timers.ElapsedEventHandler(this.TimerElapsed);
+			m_timer.Start();
+		}
+
+		private void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+		{
+			dumpMessagesToFile();
+		}
+
+		private void dumpMessagesToFile()
+		{
+			while (!m_messages.IsEmpty)
+			{
+				string temp;
+				if (m_messages.TryDequeue(out temp))
+					m_logfile.WriteLine(temp);
+			}
 		}
 
 		~Log()
 		{
-			//m_logfile.Flush();
-			m_logfile = new System.IO.StreamWriter(m_filename, false);
-			for (System.Collections.IEnumerator itr = m_messages.GetEnumerator(); itr.MoveNext();)
-				m_logfile.WriteLine(itr.Current);
-			m_logfile.Close();
+			dumpMessagesToFile();
 		}
 
 		public static String GetTimestamp()
@@ -41,9 +58,17 @@ namespace emophiz
 
 		public void Message(String message, Priority priority = Priority.Information)
 		{
-			m_messages.Enqueue(GetTimestamp() 
-				+ "\t" + System.Enum.GetName(typeof(Priority), priority) 
-				+ "\t" + message);
+			System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace(0, true);
+			System.Diagnostics.StackFrame sf = st.GetFrame(1);
+			string fl = sf.GetFileName();
+			if (fl.Length > 30)
+				fl = "..." + fl.Substring(fl.Length - 30);
+			m_messages.Enqueue(GetTimestamp()
+				+ "\t" + "File: " + fl
+				+ "\t" + "Line: " + sf.GetFileLineNumber()
+				+ "\t" + "Function: " + sf.GetMethod().Name
+				+ "\t" + "Priority: " + System.Enum.GetName(typeof(Priority), priority)
+				+ "\t" + "Message: " + message);
 		}
 	}
 }
