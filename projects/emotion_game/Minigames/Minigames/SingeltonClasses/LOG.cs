d4391894ae20abc86b2ab975937847f52c4cfd10
amin tavassolian <amin.tavassolian@gmail.com>
2012-04-17 13:51:54 -0500

    LOG class is added

---
+++ projects/emotion_game/Minigames/Minigames/SingeltonClasses/LOG.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minigames.SingeltonClasses
+{
+    public class LOG
+    {
+        //static instance to be shared
+        private static LOG instance;
+
+        //log types
+        public enum LogTypeEnum { inputLog = 0, informationLog };
+        
+        //input types
+        public enum InputDeviceTypeEnum { mouseInput = 0, keyboardinput };
+
+        //other variables
+        public LogTypeEnum _logType;
+        public InputDeviceTypeEnum _inputDevice;
+        public MINIGAMESDATA.MinigamesEnum _gameType;
+        public string _message;
+
+        //constructor
+        private LOG()
+        {
+            
+        }
+
+        //public get function
+        public static LOG Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LOG();
+                }
+                return instance;
+            }
+        }
+
+        //serialize method
+        public string SerializeToString()
+        {
+            string res = "";
+            res += _logType.ToString() + "\t";
+            if (_logType == LogTypeEnum.inputLog)
+                res += _inputDevice.ToString() + "\t";
+            res += _gameType.ToString() + "\t";
+            res += _message + "\n";
+            return res;
+        }
+    }
+}
