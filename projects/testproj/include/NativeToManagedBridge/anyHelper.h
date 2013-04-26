cc8e6a518c0986159b78adc55bfb0de5a84108ea
Faham Negini <faham.negini@gmail.com>
2013-04-26 16:41:46 -0600

    removed testproj

--- projects/testproj/include/NativeToManagedBridge/anyHelper.h
+++
@@ -1,9 +0,0 @@
-#ifndef ANYHELPER_H
-#define ANYHELPER_H
-
-#include "anyType.h"
-
-System::Object^ toObject(const AnyType &source);
-void toAny(System::Object ^source, AnyType &destination);
-
-#endif
