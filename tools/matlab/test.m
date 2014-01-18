ec703bbcc55becb89d808add1ce5d1d520c5ff9e
Faham Negini <faham.negini@gmail.com>
2014-01-18 01:58:25 -0600

    experiment report

--- tools/matlab/test.m
+++ tools/matlab/test.m
@@ -1,35 +1,12 @@
-%----------------------------------------------
-%[arousal_h, arousal_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_1\20130605_2004_arousal.csv', 3);
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\pilot_1\20130605_2004_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_1\20130605_2004_metrics.csv', 16);
 
-%[arousal_h, arousal_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_2\20130607_1729_arousal.csv', 3);
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\pilot_2\20130607_1729_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_2\20130607_1729_metrics.csv', 16);
+%-------------------------------------------------------------------------------
 
-%[arousal_h, arousal_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_3\20130611_1458_arousal.csv', 3);
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\pilot_3\20130611_1458_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_3\20130611_1458_metrics.csv', 16);
+addpath('D:/projects/emophiz/tools/matlab');
 
-%[arousal_h, arousal_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_4\20130612_1322_arousal.csv', 3);
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\pilot_4\20130612_1322_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_4\20130612_1322_metrics.csv', 16);
+root = 'D:/projects/emophiz/logs/experiment/unsplitted';
 
-%[arousal_h, arousal_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_5\20130612_1515_arousal.csv', 3);
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\pilot_5\20130612_1455_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_5\20130612_1455_metrics.csv', 16);
+%-------------------------------------------------------------------------------
 
-%[arousal_h, arousal_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_6\20130612_1703_arousal.csv', 3);
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\pilot_6\20130612_1619_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\pilot_6\20130612_1619_metrics.csv', 16);
-
-%[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\experiment\1\20130711_0921_gsr.csv', 3);
-%[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\experiment\1\20130711_0921_metrics.csv', 16);
-
-[gsr_h, gsr_v]         = readCSV('D:\faham\emophiz\emophiz\logs\experiment\10\20130711_0921_gsr.csv', 3);
-[metrics_h, metrics_v] = readCSV('D:\faham\emophiz\emophiz\logs\experiment\10\20130711_0921_metrics.csv', 16);
-
-%----------------------------------------------
 %1 time_millisecond
 %2 arousal
 %3 player_speed
@@ -46,74 +23,66 @@
 %14 medic_regen_delay
 %15 calibrating
 %16 adaptation_condition
-%----------------------------------------------
-%finding conditions
-%m_conditions = {[], [], [], []}
-%
-%m_cur_cond = 0
-%m_start = -1
-%m_end = -1
-%for i=1:length(metrics_v{16}) - 1
-%	if metrics_v{16}(i) ~= 0 && m_start == -1
-%		m_start = i
-%		m_cur_cond = metrics_v{16}(i)
-%	elseif metrics_v{16}(i) ~= 0 && (metrics_v{16}(i + 1) == 0 || i + 1 == length(metrics_v{16}))
-%		if m_end == -1
-%			m_end = i
-%			m_conditions{m_cur_cond} = [m_start, m_end]
-%		end
-%	elseif metrics_v{16}(i) == 0 && (metrics_v{16}(i + 1) ~= 0)
-%		if m_end ~= -1 && m_start ~= -1 && m_cur_cond ~= 0
-%			m_end = -1
-%			m_start = -1
-%			m_cur_cond = 0
-%		end
-%	end
-%end
-%%----------------------------------------------
-%for i=1:length(m_conditions)
-%	start_time = metrics_v{1}(m_conditions{i}(1))
-%	end_time = metrics_v{1}(m_conditions{i}(2))
-%	start_gsr_index = -1
-%	end_gsr_index = -1
-%	for j=1:length(gsr_v{1})
-%		if gsr_v{1}(j) >= start_time && start_gsr_index == -1
-%			start_gsr_index = j
-%		elseif gsr_v{1}(j) >= end_time && end_gsr_index == -1
-%			end_gsr_index = j
-%		end
-%	end
-%	%for each condition the array is as the following:
-%	%1: condition start time index
-%	%2: condition end time index
-%	%3: gsr start value
-%	%4: gsr end value
-%	%5: gsr mean
-%	%6: gsr variance
-%	%7: gsr slope
-%	m_conditions{i}(3) = gsr_v{2}(start_gsr_index)
-%	m_conditions{i}(4) = gsr_v{2}(end_gsr_index)
-%	m_conditions{i}(5) = mean(gsr_v{2}(start_gsr_index:end_gsr_index))
-%	m_conditions{i}(6) = var(gsr_v{2}(start_gsr_index:end_gsr_index), 1)
-%	m_conditions{i}(7) = diff(mean(gsr_v{2}(start_gsr_index:start_gsr_index + (end_gsr_index - start_gsr_index) / 2)), mean(gsr_v{2}(start_gsr_index + (end_gsr_index - start_gsr_index) / 2:end)))
-%end
-%----------------------------------------------
 
-plot(gsr_v{1}, (gsr_v{2} * 1000) + 800 , 'b-'); hold on;
-%plot(gsr_v{1}, smooth(gsr_v{1}, (gsr_v{2} * 1000) + 800, 0.1, 'loess'), 'r-'); hold on; 
-%plot(gsr_v{1}, gsr_v{2}, 'b.', gsr_v{1}, smooth(gsr_v{1}, (gsr_v{2} * 1000) + 800, 0.1, 'loess'),'r-')
-plot(metrics_v{1}, metrics_v{15} * 100 + 25, 'k-'); hold on; % calibration
-plot(metrics_v{1}, metrics_v{16} * 20 + 50, 'c-'); hold on; % adaptation
-%plot(bvp_v{1}, bvp_v{3}, 'c-'); %hold on;
-%plot(hr_v{1}, hr_v{3}, 'g-'); hold on; 
-%plot(arousal_v{1}, arousal_v{3}, 'r-'); hold on; 
-%plot(metrics_v{1}, metrics_v{2} * 100, 'b-'); hold on; % arousal
-plot(metrics_v{1}, (metrics_v{3}) * 100 / 2.3, 'y-'); %hold on; % player speed
-plot(metrics_v{1}, (metrics_v{4} - 1 ) * 100 / 3, 'g-'); %hold on; % zombie speed
-plot(metrics_v{1}, (metrics_v{5} - 70) * 100 / 380, 'm-'); %hold on; % fog distant
-legend('GSR Data', 'Calibration', 'Condition', 'Player Speed', 'Zombie Speed', 'Fog Distant', 'Location','NW')
-%----------------------------------------------
-xlabel('time');
-ylabel('signal');
-%grid on;
-%----------------------------------------------
+%-------------------------------------------------------------------------------
+
+file_id = {
+%    10
+     11
+%    12
+%    13
+%    14
+%    15
+%    16
+%    17
+%    18
+%    19
+%    20
+%    21
+%    23
+%    24
+%    25
+};
+
+phase_offset = 500000;
+signal_offset = 100;
+means = [];
+
+for j=1:4
+    mn = 0;
+    for i=1:length(file_id)
+        %gsr_filename            = sprintf('%s/%d_%s_%d.csv', root, file_id{i}, 'gsr', j);
+        %metrics_filename        = sprintf('%s/%d_%s_%d.csv', root, file_id{i}, 'metrics', j);
+        
+        gsr_filename            = sprintf('%s/%d_%s.csv', root, file_id{i}, 'gsr');
+        metrics_filename        = sprintf('%s/%d_%s.csv', root, file_id{i}, 'metrics');
+
+        [gsr_h, gsr_v]          = readCSV(gsr_filename, 3);
+        [metrics_h, metrics_v]  = readCSV(metrics_filename, 16);
+
+        gsr_signal_clamped = gsr_v{2} - gsr_v{2}(1);
+        gsr_time_adjusted = gsr_v{1} - gsr_v{1}(1) + phase_offset * j;
+
+        mn = mn + mean(gsr_signal_clamped) * 1000;
+        %plot(gsr_time_adjusted, smooth(gsr_v{1}, gsr_signal_clamped * 1000 + signal_offset * i, 0.1, 'loess'), 'g-'); hold on; 
+        
+        plot(gsr_v{1}, gsr_v{2} * 1000 + 700, 'b-'); hold on; % gsr
+        plot(metrics_v{1}, metrics_v{15} * 100 + 25, 'k-'); hold on; % calibration
+        plot(metrics_v{1}, metrics_v{16} * 20 + 50, 'c-'); hold on; % adaptation
+        plot(metrics_v{1}, (metrics_v{3}) * 100 / 2.3, 'y-'); %hold on; % player speed
+        plot(metrics_v{1}, (metrics_v{4} - 1 ) * 100 / 3, 'g-'); %hold on; % zombie speed
+        plot(metrics_v{1}, (metrics_v{5} - 70) * 100 / 380, 'm-'); %hold on; % fog distant
+        %legend('GSR Data', 'Calibration', 'Condition', 'Player Speed', 'Zombie Speed', 'Fog Distant', 'Location','NW')
+        
+        xlabel('time');
+        ylabel('signal');
+        %grid on;
+    end
+
+    means(j) = mn / length(file_id)
+end
+
+%bar(means, 0.4);
+
+
+%-------------------------------------------------------------------------------
